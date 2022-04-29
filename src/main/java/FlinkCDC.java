b926544ed85233086fce393f8e380f3ec11cbd28
DataBigFish <wenhaozhao0324@163.com>
2022-04-29 15:30:14 +0800

    fkinkcdc

---
+++ src/main/java/FlinkCDC.java
@@ -0,0 +1,43 @@
+import com.ververica.cdc.connectors.mysql.MySqlSource;
+import com.ververica.cdc.connectors.mysql.table.StartupOptions;
+import com.ververica.cdc.debezium.DebeziumSourceFunction;
+import com.ververica.cdc.debezium.StringDebeziumDeserializationSchema;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class FlinkCDC {
+
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        //TODO - 1.1 开启CK
+//        env.enableCheckpointing(5000);
+//        env.getCheckpointConfig().setCheckpointTimeout(10000);
+//        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+//        env.getCheckpointConfig().setMaxConcurrentCheckpoints();
+
+        // TODO - 2通过FlinkCDC构建SourceFunction
+        DebeziumSourceFunction<String> sourceFunction = MySqlSource.<String>builder()
+                .hostname("hadoop102")
+                .port(3306)
+                .username("root")
+                .password("root")
+                .databaseList("cdc_test")
+                .tableList("cdc_test.user_info")
+                .deserializer(new StringDebeziumDeserializationSchema())
+                .startupOptions(StartupOptions.initial())
+                .build();
+        DataStreamSource<String> dataStreamSource = env.addSource(sourceFunction);
+
+        // TODO - 3.直接数据打印
+        dataStreamSource.print();
+
+
+        env.execute();
+
+    }
+
+}
